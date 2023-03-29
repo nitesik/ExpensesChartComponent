a49d7ddae5d29ee581ba1742d39447ea74d5e39a
nitesik <neeteshchhetri69@gmail.com>
2023-03-29 16:10:21 +0530

    added animations

--- src/Components/Bar.js
+++ src/Components/Bar.js
@@ -10,6 +10,22 @@ function Bar({height, amount, day}) {
     {hovering && <div className="bar-amount">${amount}</div>}
     <div className="bar" style={{height: `${height}mm`}} onMouseEnter={(() => {isHovering(true)})} onMouseLeave={() => {isHovering(false)}}></div>
     <div>{day}</div>
+
+    {/* <style>{`
+      .bar {
+        animation: copy 500ms ease-in;
+      }
+
+      @keyframes copy {
+        0% {
+          height: 0;
+        }
+      
+        100% {
+          height: 100% !important;
+        }
+      }
+    `}</style> */}
   </div>)
 }
 
